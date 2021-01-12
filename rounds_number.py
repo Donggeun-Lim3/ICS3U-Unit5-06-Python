45889650fc8f0886d7dc25779b34d38659a89c07
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2021-01-12 01:27:15 +0000

    round number

---
+++ rounds_number.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+# Created by: Donggeun Lim
+# Created on: Jan 2019
+# This program rounds number
+
+
+def to_round(decimal, digit):
+
+    decimal[0] = float(decimal[0])
+    decimal[0] = decimal[0] * 10**digit
+    decimal[0] = decimal[0] + 0.5
+    decimal[0] = int(decimal[0])
+    decimal[0] = decimal[0] / 10**digit
+
+
+def main():
+    # this function rounds a number by the user to the digit the user wants
+
+    decimal = []
+    # input
+    user_decimal = input("Enter the number you want to round: ")
+    digit = int(input("Enter how many decimal places you want to round by: "))
+    decimal.append(user_decimal)
+    to_round(decimal, digit)
+    print("The rounded number is {}".format(decimal[0]))
+
+
+if __name__ == "__main__":
+    main()
